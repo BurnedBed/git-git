848311c16aaac4728b5549e6aa93e72f2834882d
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-11-19 23:19:33 -0500

    ; <= forgot

--- src/Clients/authedClient.js
+++ src/Clients/authedClient.js
@@ -25,7 +25,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateBio(bio){
         if(!bio) return await this.requestHandler.sendRequest(endPoints.self_user, `{"bio":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof bio != "string") throw new TypeError("Bio must be a string")
+        if(typeof bio != "string") throw new TypeError("Bio must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"bio":"${bio}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -35,7 +35,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateTwitterUsername(username){
         if(!username) return await this.requestHandler.sendRequest(endPoints.self_user, `{"twitter_username":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof username != "string") throw new TypeError("Username must be a string")
+        if(typeof username != "string") throw new TypeError("Username must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"twitter_username":"${username}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -45,7 +45,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateName(name){
         if(!name) return await this.requestHandler.sendRequest(endPoints.self_user, `{"name":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof name != "string") throw new TypeError("Name must be a string")
+        if(typeof name != "string") throw new TypeError("Name must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"name":"${name}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -55,7 +55,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updatePublicEmail(email){
         if(!email) return await this.requestHandler.sendRequest(endPoints.self_user, `{"email":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof email != "string") throw new TypeError("Email must be a string")
+        if(typeof email != "string") throw new TypeError("Email must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"email":"${email}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -65,7 +65,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateBlogUrl(url){
         if(!url) return await this.requestHandler.sendRequest(endPoints.self_user, `{"blog":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof url != "string") throw new TypeError("Url must be a string")
+        if(typeof url != "string") throw new TypeError("Url must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"blog":"${url}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -75,7 +75,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateCompany(company){
         if(!company) return await this.requestHandler.sendRequest(endPoints.self_user, `{"company":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof company != "string") throw new TypeError("Company must be a string")
+        if(typeof company != "string") throw new TypeError("Company must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"company":"${company}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -85,7 +85,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateLocation(location){
         if(!location) return await this.requestHandler.sendRequest(endPoints.self_user, `{"location":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof location != "string") throw new TypeError("Location must be a string")
+        if(typeof location != "string") throw new TypeError("Location must be a string");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"location":"${location}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -95,7 +95,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateHireable(hireable){
         if(!hireable) return await this.requestHandler.sendRequest(endPoints.self_user, `{"hireable":null}`, "PATCH", "application/vnd.github.v3+json");
-        if(typeof hireable != "boolean") throw new TypeError("Hireable must be boolean")
+        if(typeof hireable != "boolean") throw new TypeError("Hireable must be boolean");
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"hireable":"${hireable}"}`, "PATCH", "application/vnd.github.v3+json");
     }   
 }
