2714c3d0f66cc8788fe3c596e355c306a8ffcac8
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-11-19 23:18:59 -0500

    Err messages

--- src/Clients/authedClient.js
+++ src/Clients/authedClient.js
@@ -25,6 +25,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateBio(bio){
         if(!bio) return await this.requestHandler.sendRequest(endPoints.self_user, `{"bio":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof bio != "string") throw new TypeError("Bio must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"bio":"${bio}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -34,6 +35,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateTwitterUsername(username){
         if(!username) return await this.requestHandler.sendRequest(endPoints.self_user, `{"twitter_username":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof username != "string") throw new TypeError("Username must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"twitter_username":"${username}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -43,6 +45,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateName(name){
         if(!name) return await this.requestHandler.sendRequest(endPoints.self_user, `{"name":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof name != "string") throw new TypeError("Name must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"name":"${name}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -52,6 +55,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updatePublicEmail(email){
         if(!email) return await this.requestHandler.sendRequest(endPoints.self_user, `{"email":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof email != "string") throw new TypeError("Email must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"email":"${email}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -61,6 +65,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateBlogUrl(url){
         if(!url) return await this.requestHandler.sendRequest(endPoints.self_user, `{"blog":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof url != "string") throw new TypeError("Url must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"blog":"${url}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -70,6 +75,7 @@ module.exports = class authedClient extends baseClient{
      */
     async updateCompany(company){
         if(!company) return await this.requestHandler.sendRequest(endPoints.self_user, `{"company":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof company != "string") throw new TypeError("Company must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"company":"${company}"}`, "PATCH", "application/vnd.github.v3+json");
     }
 
@@ -79,6 +85,17 @@ module.exports = class authedClient extends baseClient{
      */
     async updateLocation(location){
         if(!location) return await this.requestHandler.sendRequest(endPoints.self_user, `{"location":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof location != "string") throw new TypeError("Location must be a string")
         return await this.requestHandler.sendRequest(endPoints.self_user, `{"location":"${location}"}`, "PATCH", "application/vnd.github.v3+json");
     }
+
+    /**
+     * 
+     * @param {Boolean} hireable 
+     */
+    async updateHireable(hireable){
+        if(!hireable) return await this.requestHandler.sendRequest(endPoints.self_user, `{"hireable":null}`, "PATCH", "application/vnd.github.v3+json");
+        if(typeof hireable != "boolean") throw new TypeError("Hireable must be boolean")
+        return await this.requestHandler.sendRequest(endPoints.self_user, `{"hireable":"${hireable}"}`, "PATCH", "application/vnd.github.v3+json");
+    }   
 }
